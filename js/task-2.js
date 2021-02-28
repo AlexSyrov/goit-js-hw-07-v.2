41163086a2ca6d4297fa77080be5770c4df74c19
AlexSyrov <1854105@gmail.com>
2021-02-28 23:51:16 +0200

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -8,8 +8,16 @@ const ingredients = [
 ];
 const ul = document.querySelector('#ingredients');
 
-const ingridientsInUl = ingredients.forEach(el => {
-    const newTag = document.createElement('li');
-    newTag.textContent = el;
-    ul.appendChild(newTag);
-})
+// const ingridientsInUl = ingredients.forEach(el => {
+//     const newTag = document.createElement('li');
+//     newTag.textContent = el;
+//     ul.appendChild(newTag);
+// })
+
+const createLi = (text) => {
+  const el = document.createElement('li');
+  el.textContent = text;
+  return el;
+}
+
+ul.prepend(...ingredients.map(createLi));
